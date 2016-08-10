8b47c4ea9740dfc7020822328620cb661c8d70fa
aidenbleakley <league@WTS7.attlocal.net>
2016-08-10 12:00:03 -0700

    E

--- src/day1/robot/intro.java
+++ src/day1/robot/intro.java
@@ -4,21 +4,19 @@ import org.jointheleague.graphical.robot.Robot;
 
 public class intro {
 public static void main(String[] args) {
-	Robot manbot=new Robot();
-Robot
+	Robot manbot=new Robot("batman");
+manbot.penDown();
 	manbot.setRandomPenColor();
-	manbot.penDown();
-	manbot.setPenWidth(1000);
-	manbot.move(100);
-manbot.turn(1000);
-manbot.move(100);
-manbot.turn(90);
-manbot.move(100);
+manbot.sparkle();
+manbot.setSpeed(10);
+manbot.move(500);
 manbot.turn(270);
-manbot.move(100);
-manbot.turn(90);
-manbot.move(100);
-manbot.turn(270);
-manbot.move(100);
+manbot.move(800);
+manbot.hide();
+	Robot mush=new Robot("vic");
+mush.move(500);
+mush.turn(90);
+
+
 }
 }
---
+++ src/day1/tortoise/class3.java
@@ -0,0 +1,14 @@
+package day1.tortoise;
+
+import javax.print.attribute.standard.JobMessageFromOperator;
+import javax.swing.JOptionPane;
+
+public class class3 {
+public static void main(String[] args) {
+System.out.print("What's up!");	
+System.out.print("my name's Aiden");
+String ans=JOptionPane.showInputDialog("Enter your name");
+JOptionPane.showInputDialog(null,ans);
+JOptionPane.showMessageDialog(null, wha);
+}
+}
