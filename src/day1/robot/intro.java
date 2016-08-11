35d5b3d9c97d7639b4122562864ef0475f199415
aidenbleakley <league@WTS7.attlocal.net>
2016-08-11 11:58:11 -0700

    robot

--- src/day1/robot/intro.java
+++ src/day1/robot/intro.java
@@ -14,9 +14,19 @@ manbot.turn(270);
 manbot.move(800);
 manbot.hide();
 	Robot mush=new Robot("vic");
-mush.move(500);
+mush.setRandomPenColor();
+	mush.penDown();
+	mush.setSpeed(10);
+	mush.move(500);
 mush.turn(90);
-
-
+mush.move(800);
+mush.hide();
+Robot dude=new Robot();
+dude.setSpeed(10);
+dude.setRandomPenColor();
+dude.penDown();
+dude.turn(180);
+dude.move(400);
+dude.turn(270);
 }
 }
--- src/day3/MadlibsOfTheAmazon.java
+++ src/day3/MadlibsOfTheAmazon.java
@@ -7,17 +7,17 @@ public class MadlibsOfTheAmazon {
 
 		// Put this sentence in a pop up:
 		// If you find yourself having to cross a piranha-infested river, here's how to do it...
-
+JOptionPane.showMessageDialog(null,"if you find your self having to cross a pirahna-infested river heres how to do it" );
 		// Get the user to enter an adjective
-
+JOptionPane.showInputDialog("enter a adjetive");
 		// Get the user to enter a type of liquid
-
+JOptionPane.showInputDialog("enter a liquid");
 		// Get the user to enter a body part
-
+JOptionPane.showInputDialog("enter a body part");
 		// Get the user to enter a verb
-
+JOptionPane.showInputDialog("enter a verb");
 		// Get the user to enter a place
-
+jop
 		// Fit the user's words into this sentence, and save it in a String:
 		// Piranhas are more [adjective] during the day, so cross the river at
 		// night. Piranhas are attracted to fresh [type of liquid] and will most
